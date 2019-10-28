4e009d539878773143491eaa737a5b25a52a37e2
Yusef Napora <yusef@protocol.ai>
2019-10-28 13:15:48 -0400

    initial commit

---
+++ crypto.go
@@ -0,0 +1,184 @@
+package go_libp2p_dtls
+
+import (
+	"crypto"
+	"crypto/ecdsa"
+	"crypto/elliptic"
+	"crypto/rand"
+	"crypto/x509"
+	"crypto/x509/pkix"
+	"encoding/asn1"
+	"errors"
+	"fmt"
+	"math/big"
+	"time"
+
+	"github.com/pion/dtls"
+
+	ic "github.com/libp2p/go-libp2p-core/crypto"
+	"github.com/libp2p/go-libp2p-core/peer"
+)
+
+const certValidityPeriod = 100 * 365 * 24 * time.Hour // ~100 years
+const certificatePrefix = "libp2p-tls-handshake:"
+
+var extensionID = getPrefixedExtensionID([]int{1, 1})
+
+type signedKey struct {
+	PubKey    []byte
+	Signature []byte
+}
+
+// Identity is used to secure connections
+type Identity struct {
+	config dtls.Config
+}
+
+// NewIdentity creates a new identity
+func NewIdentity(privKey ic.PrivKey) (*Identity, error) {
+	cert, certKey, err := keyToCertificate(privKey)
+	if err != nil {
+		return nil, err
+	}
+	return &Identity{
+		config: dtls.Config{
+			Certificate: cert,
+			PrivateKey: certKey,
+			ClientAuth: dtls.RequireAnyClientCert,
+			InsecureSkipVerify:       true, // This is not insecure here. We will verify the cert ourselves.
+			VerifyPeerCertificate: func(_ *x509.Certificate, _ bool) error {
+			  panic("dtls config not specialized for peer")
+			},
+		},
+	}, nil
+}
+
+// ConfigForAny is a short-hand for ConfigForPeer("").
+func (i *Identity) ConfigForAny() (*dtls.Config, <-chan ic.PubKey) {
+	return i.ConfigForPeer("")
+}
+
+// ConfigForPeer creates a new single-use tls.Config that verifies the peer's
+// certificate chain and returns the peer's public key via the channel. If the
+// peer ID is empty, the returned config will accept any peer.
+//
+// It should be used to create a new tls.Config before securing either an
+// incoming or outgoing connection.
+func (i *Identity) ConfigForPeer(remote peer.ID) (*dtls.Config, <-chan ic.PubKey) {
+	keyCh := make(chan ic.PubKey, 1)
+	// We need to check the peer ID in the VerifyPeerCertificate callback.
+	// The dtls.Config it is also used for listening, and we might also have concurrent dials.
+	// Clone it so we can check for the specific peer ID we're dialing here.
+	conf := dtls.Config{
+		Certificate: i.config.Certificate,
+		PrivateKey: i.config.PrivateKey,
+		ClientAuth: i.config.ClientAuth,
+		InsecureSkipVerify: i.config.InsecureSkipVerify,
+	}
+	// We're using InsecureSkipVerify, so the valid parameter will always be false.
+	conf.VerifyPeerCertificate = func(peerCert *x509.Certificate, valid bool) error {
+		defer close(keyCh)
+
+		pubKey, err := PubKeyFromCert(peerCert)
+		if err != nil {
+			return err
+		}
+		if remote != "" && !remote.MatchesPublicKey(pubKey) {
+			return errors.New("peer IDs don't match")
+		}
+		keyCh <- pubKey
+		return nil
+	}
+	return &conf, keyCh
+}
+
+// PubKeyFromCert verifies the certificate and extract the remote's public key.
+func PubKeyFromCert(cert *x509.Certificate) (ic.PubKey, error) {
+	pool := x509.NewCertPool()
+	pool.AddCert(cert)
+	if _, err := cert.Verify(x509.VerifyOptions{Roots: pool}); err != nil {
+		// If we return an x509 error here, it will be sent on the wire.
+		// Wrap the error to avoid that.
+		return nil, fmt.Errorf("certificate verification failed: %s", err)
+	}
+
+	var found bool
+	var keyExt pkix.Extension
+	// find the libp2p key extension, skipping all unknown extensions
+	for _, ext := range cert.Extensions {
+		if extensionIDEqual(ext.Id, extensionID) {
+			keyExt = ext
+			found = true
+			break
+		}
+	}
+	if !found {
+		return nil, errors.New("expected certificate to contain the key extension")
+	}
+	var sk signedKey
+	if _, err := asn1.Unmarshal(keyExt.Value, &sk); err != nil {
+		return nil, fmt.Errorf("unmarshalling signed certificate failed: %s", err)
+	}
+	pubKey, err := ic.UnmarshalPublicKey(sk.PubKey)
+	if err != nil {
+		return nil, fmt.Errorf("unmarshalling public key failed: %s", err)
+	}
+	certKeyPub, err := x509.MarshalPKIXPublicKey(cert.PublicKey)
+	if err != nil {
+		return nil, err
+	}
+	valid, err := pubKey.Verify(append([]byte(certificatePrefix), certKeyPub...), sk.Signature)
+	if err != nil {
+		return nil, fmt.Errorf("signature verification failed: %s", err)
+	}
+	if !valid {
+		return nil, errors.New("signature invalid")
+	}
+	return pubKey, nil
+}
+
+func keyToCertificate(sk ic.PrivKey) (*x509.Certificate, *crypto.PrivateKey, error) {
+	certKey, err := ecdsa.GenerateKey(elliptic.P256(), rand.Reader)
+	if err != nil {
+		return nil, nil, err
+	}
+
+	keyBytes, err := ic.MarshalPublicKey(sk.GetPublic())
+	if err != nil {
+		return nil, nil, err
+	}
+	certKeyPub, err := x509.MarshalPKIXPublicKey(certKey.Public())
+	if err != nil {
+		return nil, nil, err
+	}
+	signature, err := sk.Sign(append([]byte(certificatePrefix), certKeyPub...))
+	if err != nil {
+		return nil, nil, err
+	}
+	value, err := asn1.Marshal(signedKey{
+		PubKey:    keyBytes,
+		Signature: signature,
+	})
+	if err != nil {
+		return nil, nil, err
+	}
+
+	sn, err := rand.Int(rand.Reader, big.NewInt(1<<62))
+	if err != nil {
+		return nil, nil, err
+	}
+
+	privKey := crypto.PrivateKey(certKey)
+
+	return &x509.Certificate{
+		SerialNumber: sn,
+		NotBefore:    time.Time{},
+		NotAfter:     time.Now().Add(certValidityPeriod),
+		// after calling CreateCertificate, these will end up in Certificate.Extensions
+		ExtraExtensions: []pkix.Extension{
+			{Id: extensionID, Value: value},
+		},
+	},
+	&privKey,
+	nil
+}
